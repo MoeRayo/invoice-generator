85946b125ac657f68fca1400e2faed622f6e0368
moerayo <moronfolu.motunrayo1@gmail.com>
2022-11-17 21:35:07 +0100

    feat: creation of the home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-lightest-blue">
+    <header class="sans-serif vh-100">
+      <div class="mw9 center pa4 pt5-ns ph7-l">
+        <h3 class="f2 f1-m f-headline-l measure-narrow lh-title mv0">
+          <span class="bg-black-90 lh-copy white pa1 tracked-tight">
+           Generate a free and professional Invoice.
+          </span>
+        </h3>
+        <h4 class="f4 fw1 lh-copy georgia i">
+          Use our simplified, template to generate Invoices.
+        </h4>
+        <a
+          href="/signin"
+          class="f6 ttu tracked black-80 bg-dark-blue pa3 br3 white bb link b--black hover-bg-black bg-animate pointer"
+        >
+          Generate Invoice
+        </a>
+      </div>
+    </header>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
